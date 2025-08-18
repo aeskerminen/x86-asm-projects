264c9938532805cd546c0145969143f6f566a66d
Artturi Kerminen <kerminenartturi@gmail.com>
2025-08-18 18:30:33 +0300

    inplace reverse

--- reverse_inplace.asm
+++ reverse_inplace.asm
@@ -5,31 +5,39 @@ extern _printf
 section .bss
 
 section .data
-    string: db "test", 0
+    string: db "testt", 0
     stringLen: equ $ - string
+    fmtStr: db "%s", 0xB, 0
 
 section .text
     _main:
 
     mov esi, 0 ; init ESI to zero
-    mov edi, stringLen
-    dec edi ; init EDI to upper limit
+    mov edi, stringLen - 2
 
     loop: ; string loop
 
     cmp esi, edi ; check if index is met
-    jz end ; break loop
+    jge end ; break if esi >= edi
 
-    mov [string + esi], byte 'a' ; move 'a' to index in string
-    
-    inc esi ; inc index
+    ; swap first and last
+
+    mov al, byte [string + esi]
+    mov ah, byte [string + edi]
+
+    mov [string + esi], byte ah
+    mov [string + edi], byte al
+
+    inc esi ; inc s_index
+    dec edi ; dec e_index
 
     jmp loop ; loop again
 
     end:
     push string ; push new string to stack
+    push fmtStr ; push format
     call _printf ; print string
     
-    add esp, 0x4 ; fix stack
+    add esp, 0x8 ; fix stack
     
     ret
