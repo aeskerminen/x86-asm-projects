35399ae1cbcff4c7da9ca565d566a7ad62aee763
Artturi Kerminen <kerminenartturi@gmail.com>
2025-08-18 21:51:22 +0300

    winapi I/O

--- src/winapi_io.asm
+++ src/winapi_io.asm
@@ -9,18 +9,24 @@ section .bss
 
 section .data
     fileName: db "test.txt", 0
+    fileContent: db "I hacked your pc!! PWND!!", 0
+    fileContentLen: equ $ - fileContent
+    bytesWritten: dd 0
 
 section .text
     _createTestFile:
     push ebp
     mov ebp, esp
     
+    ; hTemplateFile
+    push 0
+
     ; dwFlagsAndAttributes
     mov eax, 0x80
     push eax
 
     ; dwCreationDisposition
-    mov eax, 1
+    mov eax, 2
     push eax
 
     ; lpSecurityAttributes
@@ -32,7 +38,7 @@ section .text
     push eax
 
     ; dwDesiredAccess
-    mov eax, 0x4
+    mov eax, 0x40000000
     push eax
 
     ; lpFileName
@@ -43,12 +49,50 @@ section .text
     pop ebp
     ret
 
+    _writeToTestFile:
+    push ebp
+    mov ebp, esp
+
+    ; store the handle in ebx and push to stack
+    mov ebx, [esp + 8]
+
+    ; lpOverlapped
+    push 0
+
+    ; lpNumberOfBytesWritten
+    push bytesWritten
+
+    ; nNumberOfBytesToWrite
+    push fileContentLen
+
+    ; LpBuffer
+    push fileContent
+
+    ; hFile
+    push ebx
+
+    call _WriteFile@20
+
+    pop ebp
+    ret
+    
     _main:
    
+    ; create file test.txt
     call _createTestFile
+
+    ; write to file test.txt
+    push eax ; handle
+    call _writeToTestFile
+    add esp, 0x4
+
+    ; restore handle to file
+    mov eax, ebx
 
     ; close unused handle to the file
     push eax
-    call _CloseHandle@4 ;
+    call _CloseHandle@4
+
+    add esp, 0x4
 
     ret
