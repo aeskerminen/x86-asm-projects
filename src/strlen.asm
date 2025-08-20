875dcf13f4f01cb6507ff8115b0b87d0578adfbc
Artturi Kerminen <kerminenartturi@gmail.com>
2025-08-20 12:24:56 +0300

    strlen

---
+++ src/strlen.asm
@@ -0,0 +1,40 @@
+global _main
+
+extern _printf
+
+section .bss
+
+section .data
+    fmtStr: db "The length of: %s is %d", 0xB, 0
+
+section .text
+    _main:
+
+    ; read argv into memory
+    mov eax, [esp + 8]
+    ; read first argument
+    mov ebx, [eax + 1 * 4]
+
+    ; len
+    mov ecx, 0
+    ; index
+    mov esi, 0
+
+    loop:
+    ; check for null terminator
+    cmp byte [ebx + esi], 0x00
+    jz end
+
+    ; increment index and len
+    inc ecx
+    inc esi
+    
+    jmp loop
+
+    end: ; print length
+    push ecx
+    push ebx
+    push fmtStr
+    call _printf
+    
+    ret
