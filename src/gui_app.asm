594aaa3f0cda6923a8b5073d506386c85b22b064
Artturi Kerminen <kerminenartturi@gmail.com>
2025-08-22 20:31:01 +0300

    Drawing child windows (components). Thanks David Grantham!

--- src/gui_app.asm
+++ src/gui_app.asm
@@ -15,6 +15,51 @@ SW_SHOWNORMAL       EQU 1
 WM_DESTROY          EQU 2
 WS_EX_COMPOSITED    EQU 2000000h
 WS_OVERLAPPEDWINDOW EQU 0CF0000h
+ANSI_CHARSET         EQU 0                      
+BLACKNESS            EQU 42h
+CLIP_DEFAULT_PRECIS  EQU 0
+CS_BYTEALIGNWINDOW   EQU 2000h
+CS_HREDRAW           EQU 2
+CS_VREDRAW           EQU 1
+DEFAULT_PITCH        EQU 0
+ES_AUTOHSCROLL       EQU 80h
+ES_CENTER            EQU 1
+FALSE                EQU 0
+GRAY_BRUSH           EQU 2
+IDC_ARROW            EQU 7F00h
+IDI_APPLICATION      EQU 7F00h
+IDNO                 EQU 7
+IMAGE_CURSOR         EQU 2
+IMAGE_ICON           EQU 1
+LR_SHARED            EQU 8000h
+MB_DEFBUTTON2        EQU 100h
+MB_YESNO             EQU 4
+NULL                 EQU 0
+NULL_BRUSH           EQU 5
+OPAQUE               EQU 2
+PROOF_QUALITY        EQU 2
+SM_CXFULLSCREEN      EQU 10h
+SM_CYFULLSCREEN      EQU 11h
+SS_CENTER            EQU 1
+SS_NOTIFY            EQU 100h
+SW_SHOWNORMAL        EQU 1
+TRUE                 EQU 1
+WM_CLOSE             EQU 10h
+WM_COMMAND           EQU 111h
+WM_CREATE            EQU 1
+WM_CTLCOLOREDIT      EQU 133h
+WM_CTLCOLORSTATIC    EQU 138h
+WM_DESTROY           EQU 2
+WM_PAINT             EQU 0Fh
+WM_SETFONT           EQU 30h
+OUT_DEFAULT_PRECIS   EQU 0
+WS_CHILD             EQU 40000000h
+WS_EX_COMPOSITED     EQU 2000000h
+WS_OVERLAPPEDWINDOW  EQU 0CF0000h
+WS_TABSTOP           EQU 10000h
+WS_VISIBLE           EQU 10000000h
+
+Static1ID            EQU 100
 
 extern _CreateWindowExA@48
 extern _ShowWindow@8
@@ -36,11 +81,14 @@ extern _printf
 
 section .bss
     hInstance resd 1
+    Static1 resd 1
 section .data
     windowName: db "Assembly GUI", 0
     className: db "Asddsembly class", 0
     failMsg: db "Program failed", 0
     errMsg: db "Error code: %d", 0xB, 0
+    StaticClass: db "STATIC", 0
+    Text1: db "Hello, world!", 0
 
     wc: ; WndClassEX
         dd 48 ; cbSize
@@ -70,6 +118,9 @@ section .text
         cmp   dword [uMsg], WM_DESTROY                 ; [EBP + 12]
         je    WMDESTROY
 
+        cmp dword [uMsg], WM_CREATE
+        je WMCREATE
+
         DefaultMessage:
         push  dword [lParam]                           ; [EBP + 20]
         push  dword [wParam]                           ; [EBP + 16]
@@ -84,7 +135,28 @@ section .text
         WMDESTROY:
         push  NULL
         call  _PostQuitMessage@4
+        jmp _WM_PROCESSED
 
+        WMCREATE:
+        ; do stuff
+        push  NULL
+        push  dword [hInstance]
+        push  Static1ID
+        push  dword [hWnd]                             ; [EBP + 8]
+        push  20                                       ; Height
+        push  400                                      ; Width
+        push  10                                       ; Y
+        push  120                                      ; X
+        push  WS_CHILD | WS_VISIBLE | SS_NOTIFY | SS_CENTER
+        push  Text1                                    ; Default text
+        push  StaticClass
+        push  NULL
+        call  _CreateWindowExA@48
+        mov   dword [Static1], EAX
+        jmp _WM_PROCESSED
+
+
+        _WM_PROCESSED:
         xor   EAX, EAX                                 ; WM_DESTROY has been processed, return 0
         mov   ESP, EBP                                 ; Remove the stack frame
         pop   EBP
